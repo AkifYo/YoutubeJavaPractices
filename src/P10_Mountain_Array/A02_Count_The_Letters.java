2d8152e7a6ce6fa3672dcc4526c5dc27c50b8f6a
akif <70863898+akif-ned@users.noreply.github.com>
2023-08-31 21:20:50 +0200

    31/08-akif

--- src/P10_Mountain_Array/A02_Count_The_Letters.java
+++ src/P10_Mountain_Array/A02_Count_The_Letters.java
@@ -9,22 +9,22 @@ public class A02_Count_The_Letters {
         // in this chapter we are gonna make an function that counts how much a letters in the text
         Scanner scan = new Scanner(System.in);
         System.out.println("Please type an text.");
-        String userText= scan.nextLine();
-        findTheLetter(userText);
+        String uText= scan.nextLine();
+        findTheLetter(uText);
     }
 
-    private static void findTheLetter(String userText) {
+    private static void findTheLetter(String uText) {
         List<String> listCharacter= new ArrayList<>();
         int count=0;
-        for (int i = 0; i < userText.length(); i++) {
-            for (int j = i+1; j < userText.length(); j++) {
-                if (userText.charAt(i)==userText.charAt(j)){
+        for (int i = 0; i < uText.length(); i++) {
+            for (int j = i+1; j < uText.length(); j++) {
+                if (uText.charAt(i)==uText.charAt(j)){
                     count++;
                 }
             }
-            if (count>0 && !listCharacter.contains(userText.substring(i,i+1)) &&
-                    !userText.substring(i,i+1).contains(" ")) {
-                listCharacter.add(userText.substring(i,i+1));
+            if (count>0 && !listCharacter.contains(uText.substring(i,i+1)) &&
+                    !uText.substring(i,i+1).contains(" ")) {
+                listCharacter.add(uText.substring(i,i+1));
 
             }
             count=0;
